4d67fafa8552a79fe4cd2c653d602e84393abe97
shayaankn <shayaankhan11@gmail.com>
2026-01-18 20:58:00 +0530

    update initialData.js

--- src/data/initialData.js
+++ src/data/initialData.js
@@ -1,7 +1,7 @@
 export const initialData = {
   columns: {
     applied: { id: "applied", title: "Applied", cardIds: [] },
-    interview: { id: "interview", title: "Interview Scheduled", cardIds: [] },
+    interview: { id: "interview", title: "Interview", cardIds: [] },
     progress: { id: "progress", title: "In Progress", cardIds: [] },
     rejected: { id: "rejected", title: "Rejected", cardIds: [] },
     offer: { id: "offer", title: "Offer Received", cardIds: [] },
